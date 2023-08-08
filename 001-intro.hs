4ae001cb946a080735073eb636be9689cc1c9be7
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2023-08-08 12:52:15 -0500

    First commit.

---
+++ 001-intro.hs
@@ -0,0 +1,9 @@
+module Test where
+
+-- Divisón entera entre dos números
+division :: Int -> Int -> Int
+division x y = x `div` y
+
+-- Función para encontrar el cuadrado de un número
+cuadrado :: Int -> Int
+cuadrado x = x * x
